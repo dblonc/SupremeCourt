8a4f544b6e6b7da2882eba21005d9f1230e63e1d
Daniel Bloncourt <dblonc@aol.com>
2022-04-15 18:29:26 -0400

    formatted data for judges and cases

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -6,17 +6,20 @@ class Data extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            judges: {}
+            judges: {},
+            cases: {}
         }
         this.fetchJudges = this.fetchJudges.bind(this)
         this.formatJudges = this.formatJudges.bind(this)
+        this.fetchCases = this.fetchCases.bind(this)
+        this.formatCases = this.formatCases.bind(this)
     }
 
 
     fetchJudges(){
-        const judges = fetch("https://frontend-exercise-api.herokuapp.com/justices/")
-                        .then((response)=>response.json())
-                        .then((data)=> this.formatJudges(data))
+        return fetch("https://frontend-exercise-api.herokuapp.com/justices/")
+            .then((response)=>response.json())
+            .then((data)=> this.formatJudges(data))
       
     }
 
@@ -33,17 +36,55 @@ class Data extends React.Component {
             judges: tempJudges
         })
 
-        console.log(tempJudges)
     }
 
 
 
+    fetchCases(){
+        let year = 1795;
+        while(year < 2022){
+               let cases = fetch(`https://frontend-exercise-api.herokuapp.com/cases/?before=${year + 5}1231&after=${year}0101`)
+                    .then((response) => response.json())
+                    .then((data) => this.formatCases(Object.values(data)))
+                year += 20
+            
+        }
+        
+    }
+
+    formatCases(data) {
+
+        let newJudges = {...this.state.judges};
+        data.forEach(singleCase =>{
+            singleCase.dissents.forEach(dissent => {
+                newJudges[dissent].caseCount += 1
+            })
+            singleCase.majority.forEach(major => {
+                newJudges[major].caseCount += 1
+            })
+            singleCase.other.forEach(oth => {
+                newJudges[oth].caseCount += 1
+            })
+        })
+
+        this.setState({
+            judges: newJudges
+        })        
+    }
+
+    // componentDidMount(){
+    //     this.fetchJudges().then(()=>this.fetchCases())
+    // }
+
     render(){
+        console.log(this.state.judges)
         return(
             <div className="data-container">
                 <div className = "data-title">Data</div>
                 <div className="data-area">
                     <button onClick={this.fetchJudges}>Judges</button>
+                    <button onClick={this.fetchCases}>Cases</button>
+                    <svg width='500' height ='400'></svg>
                 </div>
             </div>
         )
